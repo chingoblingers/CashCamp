cb95a027e7caa979cc5f7b2b5c3d7ef0a697b1a6
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-06 20:00:16 -0500

    added middlewear for reading json + test route to confirm post requests work

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import express from "express"
 const app = express()
 const PORT = 8000
 
+app.use(express.json())
+
+app.post("/test", (req, res)=>{
+  res.json({message: 'recieved post'})
+})
+
 app.get("/test", (req, res) => {
   res.json({ message: "CashCamp backend running" })
 })
