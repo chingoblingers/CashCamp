456feca54191831d3b44430c123e7270cef050f7
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-14 21:28:28 -0500

    updated env variable and test route function for testing get request to server

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,9 +1,11 @@
-
+import {pool} from "./../db/db.js"
 
 export function postController(req, res){
     res.json({recived: req.body})
 }
 
-export function getController(req, res){
-    res.json({ message: "CashCamp backend running" })
+export async function getController(req, res){
+    const result = await pool.query("SELECT NOW()")
+    res.json(result.rows)
 }
+
