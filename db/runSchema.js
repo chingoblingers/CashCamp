6f7cf8f8443dbe361c1c5432228a97553355d61c
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-17 19:11:34 -0500

    created function for reading and creating the sql tables in the schema

--- db/runSchema.js
+++ db/runSchema.js
@@ -8,3 +8,22 @@ const accountsSql = path.join(__dirname, "../sql", "accounts.sql")
 const categoriesSql = path.join(__dirname, "../sql", "categories.sql")
 const transactionsSql = path.join(__dirname, "../sql", "transactions.sql")
 const sqlFiles = [usersSql,accountsSql,categoriesSql,transactionsSql]
+
+async function runSchema(){
+
+try{
+ for (let file of sqlFiles) {
+    const sql = await fs.readFile(file, "utf-8")
+    await pool.query(sql)
+    console.log(`${file} executed successfully`)
+  }
+}catch(error){
+console.error(`unable to read file due to error: ${error}`)
+}finally{
+ await pool.end()
+ console.log("Schema executed successfully")
+}    
+}
+
+runSchema()
+
