d3099995f7957d3ba8b4fa21532ba38a8681c6f3
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-17 20:23:57 -0500

    created function for user inserts

---
+++ queries/usersQueries.js
@@ -0,0 +1,12 @@
+import { pool } from "./../db/db.js"
+
+export async function createUser(name, email) {
+  const result = await pool.query(
+    `INSERT INTO users (name, email)
+     VALUES ($1, $2)
+     RETURNING *`,
+    [name, email]
+  )
+
+  return result.rows[0]
+}
