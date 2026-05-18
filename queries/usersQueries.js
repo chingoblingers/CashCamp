3e39ee40ab6ca547995f9c3dd32abf877c2cf4d9
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-17 20:42:14 -0500

    destructured createUser query so i can remember it is an object and created getUsers query function

--- queries/usersQueries.js
+++ queries/usersQueries.js
@@ -1,12 +1,17 @@
 import { pool } from "./../db/db.js"
 
 export async function createUser(name, email) {
-  const result = await pool.query(
+  const {rows} = await pool.query(
     `INSERT INTO users (name, email)
      VALUES ($1, $2)
      RETURNING *`,
     [name, email]
   )
 
-  return result.rows[0]
+  return rows[0]
+}
+
+export async function getUsers(){
+    const {rows} = await pool.query("SELECT * FROM users")
+    return rows
 }
