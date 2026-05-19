f4869b4ff28bde21c9ed4da3c9ba1f039b71757a
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-18 21:03:34 -0500

    created update user query, controller and route

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getAllUsers, createNewUser, getUser } from "../controllers/usersController.js"
+import { getAllUsers, createNewUser, getUser, updateUser } from "../controllers/usersController.js"
 
 const usersRouter = express.Router()
 
 usersRouter.get("/", getAllUsers)
 usersRouter.post("/", createNewUser)
 usersRouter.get("/:id", getUser)
+usersRouter.put("/:id", updateUser)
 
 export default usersRouter
