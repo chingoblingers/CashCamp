4d3dcae3c288937704aa7d8230eb48836e5ca255
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-19 19:42:59 -0500

    created accountsRouter and imported into server.js middlewear

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import TestRouter from "./routes/testRoutes.js"
 import UsersRouter from "./routes/usersRoutes.js"
+import AccountsRouter from "./routes/accountsRoutes.js"
 
 const app = express()
 const PORT = 8000
@@ -8,6 +9,7 @@ const PORT = 8000
 app.use(express.json())
 app.use("/test", TestRouter)
 app.use("/users", UsersRouter)
+app.use("/users/:id/accounts", AccountsRouter)
 
 app.listen(PORT, () => {
   console.log(`running on PORT:${PORT} successfully`)
