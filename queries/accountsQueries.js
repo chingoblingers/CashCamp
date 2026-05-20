132ba6af295638a5a3f2bf9020a14fde34daef55
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-05-19 22:35:26 -0500

    created updateAccount controller, Route and Query.

--- queries/accountsQueries.js
+++ queries/accountsQueries.js
@@ -9,3 +9,15 @@ export async function createSingleAccount(id, name, type, balance){
     const {rows} = await pool.query("INSERT INTO accounts (user_id, account_name, account_type, starting_balance) VALUES ($1,$2,$3,$4) RETURNING *", [id, name, type, balance])
     return rows[0]
 }
+
+export async function updateUserAccount(name, type, balance, userId, accountId) {
+  const { rows } = await pool.query(
+    `UPDATE accounts
+     SET account_name = $1, account_type = $2, starting_balance = $3
+     WHERE user_id = $4 AND id = $5
+     RETURNING *`,
+    [name, type, balance, userId, accountId]
+  )
+
+  return rows[0]
+}
