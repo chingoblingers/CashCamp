ebbdc03e55e8a20b2c1601c9e6df717f158238ab
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-06-05 05:36:57 -0500

    created get all categories route.

--- queries/categoriesQueries.js
+++ queries/categoriesQueries.js
@@ -4,3 +4,8 @@ export async function createSingleCategory(id, name, kind, group){
     const {rows} = await pool.query("INSERT INTO categories (user_id, name, kind, category_group) VALUES ($1, $2, $3, $4) RETURNING *", [id,name,kind,group])
     return rows[0]
 }
+
+export async function getCategories(id){
+    const {rows} = await pool.query("SELECT * FROM categories WHERE user_id = $1", [id])
+    return rows
+}
