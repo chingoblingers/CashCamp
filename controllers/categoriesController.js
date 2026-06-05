c4c98cb272906eefa314d7b9455efa392080d05f
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-06-05 05:36:57 -0500

    created get all categories route.

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,4 +1,4 @@
-import { createSingleCategory } from "../queries/categoriesQueries.js";
+import { createSingleCategory, getCategories } from "../queries/categoriesQueries.js";
 
 export async function createCategory(req,res){
     try{
@@ -14,6 +14,18 @@ export async function createCategory(req,res){
      console.error(error)
     }
     
+}
 
-
+export async function getUserCatergories(req, res) {
+    try{
+        const {userId} = req.params
+        const categories = await getCategories(userId)
+        if (categories.length === 0){
+            return res.status(404).json({message: "not finding any categoreis broski"})
+        }
+        res.status(200).json({categories: categories})
+    }catch(error){
+        res.status(500).json({error: error.message})
+        console.error(error)
+    }
 }
--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { createCategory } from "../controllers/categoriesController.js"
+import { createCategory, getUserCatergories } from "../controllers/categoriesController.js"
 
 const categoriesRouter = express.Router({ mergeParams: true })
 
 categoriesRouter.post("/" , createCategory)
+categoriesRouter.get("/", getUserCatergories)
 
 export default categoriesRouter
