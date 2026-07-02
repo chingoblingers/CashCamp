be4cd67168074273dae415665a16bbfd09ee5d48
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-01 21:18:29 -0500

    created user summary controller and route + refactored server.js route to use both summary router controllers

--- routes/summaryRoutes.js
+++ routes/summaryRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { handleAccountSummary } from "../controllers/summaryController.js"
+import { handleAccountSummary, handleUserSummary } from "../controllers/summaryController.js"
 
 const summaryRouter = express.Router({mergeParams: true})
 
-summaryRouter.get("/", handleAccountSummary)
+summaryRouter.get("/summary", handleUserSummary)
+summaryRouter.get("/accounts/:accountId/summary", handleAccountSummary)
 
 export default summaryRouter
