fe97c71036242acbc1635f33fca6f45a9d4aa0bd
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-03 04:22:10 -0500

    made createUser for authQuery

--- queries/authQueries.js
+++ queries/authQueries.js
@@ -1,1 +1,6 @@
-test
+import { pool } from "../db/db.js";
+
+export async function createUser(name, email, passwordHash){
+    const {rows} = await pool.query(`INSERT into users (name, email, password_hash) VALUES($1, $2, $3) RETURNING id, name, email`, [name, email, passwordHash])
+    return rows[0]
+}
