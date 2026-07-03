f590b4ae713c8c3aadc11f5fb54ff55afd423854
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-03 04:34:16 -0500

    created findUserByEmail Query

--- queries/authQueries.js
+++ queries/authQueries.js
@@ -4,3 +4,8 @@ export async function createUser(name, email, passwordHash){
     const {rows} = await pool.query(`INSERT into users (name, email, password_hash) VALUES($1, $2, $3) RETURNING id, name, email`, [name, email, passwordHash])
     return rows[0]
 }
+
+export async function findUserByEmail(email){
+    const {rows} = await pool.query(`SELECT id, name, email, password_hash from users WHERE email = $1`,[email])
+    return rows[0]
+}
