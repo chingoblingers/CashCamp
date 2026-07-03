8bcad6e65a6ae0f94701c5d3f0c482060dea2b87
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-03 01:41:29 -0500

    added password_hash to users table in preperation for setting up auth

--- sql/users.sql
+++ sql/users.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS users(
 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 name TEXT NOT NULL, 
 email TEXT NOT NULL UNIQUE,
+password_hash TEXT NOT NULL,
 created_at TIMESTAMPTZ DEFAULT NOW()
 );
