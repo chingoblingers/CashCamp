369c67c3004cc72c1dc9e987b4e6bb3d051312fe
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-05 05:17:47 -0500

    created handleLogin function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-import { createUser } from "../queries/authQueries.js";
+import { createUser, findUserByEmail } from "../queries/authQueries.js";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export async function handleSignup(req, res){
     try{
@@ -22,3 +23,26 @@ export async function handleSignup(req, res){
     }
 }
 
+export async function handleLogin(req, res){
+    try{
+    const {email, password} = req.body
+    if (!email || !password){
+        return res.status(400).json({message: "missing a required field"})
+    }
+    const user = await findUserByEmail(email)
+    if(!user){
+        return res.status(401).json({message: "Invalid email or password"})
+    }
+    const {password_hash} = user
+    const isValid = await bcrypt.compare(password, password_hash)
+    if(!isValid){
+        return res.status(401).json({message: "Invalid email or password"})
+    }
+    const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: "1h"})
+    const verifiedUser = {id: user.id, name: user.name, email: user.email}
+    res.status(200).json({token, user: verifiedUser })
+    }catch(error){
+        console.error(error)
+        res.status(500).json({error: error.message})
+    }
+}
