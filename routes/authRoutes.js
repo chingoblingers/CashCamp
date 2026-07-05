0b9cba8e5794ec23a0e3e0c0e3c6c47c90f266a4
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-05 05:18:58 -0500

    created Login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
-import { handleSignup } from "../controllers/authController.js";
+import { handleSignup, handleLogin } from "../controllers/authController.js";
 import express from "express"
 
 const authRouter = express.Router()
 
 authRouter.post("/signup", handleSignup)
+authRouter.post("/login", handleLogin)
 
 export default authRouter
