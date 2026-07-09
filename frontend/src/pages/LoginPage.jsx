d30b519b2882a87570043b8bab2fb1c28d2d5f0c
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-09 18:56:23 -0500

    added signup heading and created links to signup and login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,9 +3,8 @@ import {Link} from 'react-router-dom'
 export default function LoginPage(){
     return (
         <>
-        <h1> Welcome to CashCamp the worlds best finace tracker! </h1>
-        <Link to="/signup"> Click here if you dont have an account! </Link>
-
+        <h1>Welcome to CashCamp, the world's best finance tracker!</h1>
+        <Link to="/signup">Click here if you don't have an account!</Link>
         </>
     )
 }
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,5 +1,10 @@
+import {Link} from 'react-router-dom'
+
 export default function SignupPage(){
     return (
-        <h1> Signup Page </h1>
+        <>
+        <h1> Sign up here to create your account! </h1>
+        <Link to="/login"> click here to login if you have an account already! </Link>
+        </>
     )
 }
