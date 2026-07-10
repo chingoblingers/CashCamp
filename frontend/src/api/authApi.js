97d4dd63724ebe68a93dbc19bbffaa538fbffeea
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-09 20:39:55 -0500

    crorrected function

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,5 +1,5 @@
 
-function aync signupUser(name, email, password){
+export async function signupUser(name, email, password){
 const res = await fetch("http://localhost:8000/auth/signup", {method: "POST", headers:{"Content-Type": "application/json"}, body: JSON.stringify({name, email, password})})
 const data = res.json()
 return data
