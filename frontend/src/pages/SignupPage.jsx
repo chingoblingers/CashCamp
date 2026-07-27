dff57347e347b52cd617d488f40bd571a3cda583
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-07-27 17:07:13 -0500

    added navigate and error state, refactored handlesubmit and added error message to JSX

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {useState} from 'react'
 import {signupUser} from './../api/authApi.js'
 
@@ -6,11 +6,18 @@ export default function SignupPage(){
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
 
    async function handleSubmit(e){
         e.preventDefault()
+        setError("")
      const data = await signupUser(name, email, password)
-     console.log(data)
+    if(!data.user){
+    setError(data.message || "Signup Failed")
+    return
+    } 
+    navigate("/login")
     }
 
     return (
@@ -21,10 +28,11 @@ export default function SignupPage(){
         <label htmlFor="signupName">Name</label>    
         <input type="text" name="name" id="signupName" placeholder="Tyler" required onChange={(e)=>setName(e.target.value)} value={name}/>
         <label htmlFor="signupEmail">Email</label>
-        <input type="email" name="signupEmail" id="email" placeholder="tyler@emailsite.com" required onChange={(e)=>setEmail(e.target.value)} value={email}/>
+        <input type="email" name="email" id="signupEmail" placeholder="tyler@emailsite.com" required onChange={(e)=>setEmail(e.target.value)} value={email}/>
         <label htmlFor="signupPassword">Password</label>
-        <input type="password" name="signupPassword" id="password" placeholder="Password123" required onChange={e=>setPassword(e.target.value)} value={password}/>
+        <input type="password" name="password" id="signupPassword" placeholder="Password123" required onChange={e=>setPassword(e.target.value)} value={password}/>
         <button type="submit"> Submit </button>
+        {error && <p>{error}</p>}
         </form>    
         </>
     )
