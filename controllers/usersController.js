5c1d783385dc26ae0b00afaae3f7e1734d58efb4
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-08-02 23:44:59 -0500

    changed delete user status code

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -60,7 +60,7 @@ const deletedUser = await deleteSingleUser(id)
 if (!deletedUser){
     return res.status(404).json({error: "unable to delete user"})
 }
-res.status(200).json({message: "User deleted successfully", user: deletedUser})
+res.status(201).json({message: "User deleted successfully", user: deletedUser})
 }catch(error){
 console.error(error)
 res.json(500).json({error: error.message})    
