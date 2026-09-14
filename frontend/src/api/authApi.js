b99686c314e9cd9c5757f1930fb1ff7cd31899f2
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-14 10:56:08 -0500

    created loginUser function

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -5,3 +5,8 @@ const data = res.json()
 return data
 }
 
+export async function loginUser(email, password){
+    const response = await fetch(`http://localhost:8000/auth/login`, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({email, password})})
+    const data = await response.json()
+    return data
+}
