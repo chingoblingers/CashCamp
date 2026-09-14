cd58d7f54fe37027ed57591ecf6eacc7cefa396f
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-14 10:27:40 -0500

    created login page skeleton

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,39 @@
 import {Link} from 'react-router-dom'
+import { useState } from 'react'
 
 export default function LoginPage(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    
+   async function handleSubmit(e){
+        try{
+            e.preventDefault()
+
+        }catch(error){
+
+        }
+        
+        
+    }
+    
     return (
         <>
-        <h1>Welcome to CashCamp, the world's best finance tracker!</h1>
-        <Link to="/signup">Click here if you don't have an account!</Link>
+        <header>
+            <h1>Welcome to CashCamp, the world's best finance tracker!</h1>
+        </header>
+        <main>
+            <Link to="/signup">Click here if you don't have an account!</Link>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='loginEmail'>Email:</label>
+                <input type='email' id='loginEmail' name='email' placeholder='yamir@gmail.com' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <label htmlFor='loginPass'>Password:</label>
+                <input type='password' id='loginPass' name='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                <button type='submit'> Login </button> 
+            </form>
+            {error && <p>{error}</p>}   
+        </main>
         </>
     )
 }
