064e82dc75a510a6b3abc95720e6610d7685709f
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-14 17:39:55 -0500

    created auth context and provider function.

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,1 +1,15 @@
 import { useState, createContext } from "react";
+
+export const AuthContext = createContext()
+
+ export function AuthProvider({children}){
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token') ? true: false)
+
+    return(
+        <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+            {children}
+        </AuthContext.Provider>
+
+    )
+}
+
