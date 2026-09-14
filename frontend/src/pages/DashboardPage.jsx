80627f913a20d093d088f13dfb8ddca74805835b
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-14 11:53:08 -0500

    imported dashboard Summary function and set up intial states.

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,4 +1,13 @@
+import { getDashboardSummary } from "../api/summaryApi.js"
+import { useState } from "react"
+
 export default function DashboardPage(){
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [dashboardData, setDashboardData] = useState(null)
+
+
+
     return (
         <h1> Dashboard Page </h1>
     )
