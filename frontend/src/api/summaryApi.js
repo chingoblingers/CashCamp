21f4c60a428dbe64d9715671784540bffd4a865d
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-14 18:15:51 -0500

    added custom error so error data is not sent to frontend.

--- frontend/src/api/summaryApi.js
+++ frontend/src/api/summaryApi.js
@@ -3,6 +3,9 @@
 export async function getDashboardSummary(){
     const token = localStorage.getItem('token')
     const response = await fetch(`http://localhost:8000/me/summary`, {method: 'GET', headers:{Authorization: `Bearer ${token}`}})
+    if (response.status === 401){
+        throw new Error('Unauthorized')
+    }
     const data = await response.json()
     return data
 }
