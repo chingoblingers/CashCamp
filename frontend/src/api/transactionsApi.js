14c4181bbb006d64eb8be1e3be9ab132af36c5c3
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-15 18:52:55 -0500

    created delete transaction function.

--- frontend/src/api/transactionsApi.js
+++ frontend/src/api/transactionsApi.js
@@ -11,3 +11,16 @@ export async function createTransaction(accountId, categoryId, amount, descripti
     const data = await response.json()
     return data.transaction
 }
+
+export async function deleteTransaction(accountId, transactionId){
+    const token = localStorage.getItem("token")
+    const response = await fetch(`http://localhost:8000/me/accounts/${accountId}/transactions/${transactionId}`, {method: 'DELETE', headers:{Authorization: `Bearer ${token}`}})
+    if (response.status === 401){
+        throw new Error('Unauthorized')
+    }
+    if (!response.ok){
+        throw new Error('Unable to complete request.')
+    }
+    const data = await response.json()
+    console.log(data.deleted)
+}
