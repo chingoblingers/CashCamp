b83209c3df9125b709ff41fbe4503ee7d2fd96db
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-15 18:31:55 -0500

    created delete category function.

--- frontend/src/api/categoriesApi.js
+++ frontend/src/api/categoriesApi.js
@@ -24,3 +24,16 @@ export async function createCategory(name, kind, group){
     const data = await response.json()
     return data.created
 }
+
+export async function deleteCategory(categoryId){
+    const token = localStorage.getItem('token')
+    const response = await fetch(`http://localhost:8000/me/categories/${categoryId}`, {method: 'DELETE', headers:{Authorization: `Bearer ${token}`}})
+    if (response.status === 401){
+        throw new Error('Unauthorized')
+    }
+    if (!response.ok){
+        throw new Error('Unable to complete request.')
+    }
+    const data = await response.json()
+    console.log(data.message)
+}
