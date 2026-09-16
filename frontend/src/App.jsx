7de66afa7928054f2b56ed26c4d9582f5d12d657
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-16 14:47:23 -0500

    looked up a way to stop background image from flickering. added a preload useEffect.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,17 @@ import LoginPage from './pages/LoginPage.jsx'
 import SignupPage from './pages/SignupPage.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 import HomePage from './pages/HomePage.jsx'
+import { useEffect } from 'react'
+import plant1 from './assets/plant1.jpg'
+import plant2 from './assets/plant2.jpg'
+import plant3 from './assets/plant3.jpg'
+
+useEffect(() => {
+     [plant1, plant2, plant3].forEach(src => {
+        const img = new Image()
+        img.src = src
+    })
+}, [])
 
 const routesArr = [
   {path: "/", element: <HomePage/>},
