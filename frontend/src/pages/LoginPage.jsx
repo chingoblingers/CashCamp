75662725b31fddb331151717d1ec42188831366b
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-16 14:34:24 -0500

    styled the login page signup card.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -35,18 +35,20 @@ export default function LoginPage(){
         <>
 
         <main className='loginPage'>
-            <header>
-                <h1>Welcome to CashCamp, the world's best finance tracker!</h1>
-            </header>
-            <Link to="/signup">Click here if you don't have an account!</Link>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='loginEmail'>Email:</label>
-                <input type='email' id='loginEmail' name='email' placeholder='yamir@gmail.com' value={email} onChange={(e)=> setEmail(e.target.value)}/>
-                <label htmlFor='loginPass'>Password:</label>
-                <input type='password' id='loginPass' name='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
-                <button type='submit'> Login </button> 
-            </form>
-            {error && <p>{error}</p>}   
+            <div className='authCard'>
+                <header>
+                    <h1>Welcome to CashCamp, the world's best finance tracker!</h1>
+                </header>
+                <Link to="/signup">Click here if you don't have an account!</Link>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='loginEmail'>Email:</label>
+                    <input type='email' id='loginEmail' name='email' placeholder='yamir@gmail.com' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                    <label htmlFor='loginPass'>Password:</label>
+                    <input type='password' id='loginPass' name='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                    <button type='submit'> Login </button> 
+                </form>
+                {error && <p>{error}</p>}  
+            </div> 
         </main>
         </>
     )
