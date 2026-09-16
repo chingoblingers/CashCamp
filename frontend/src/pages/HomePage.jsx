3276f594208cbf32e850257cf049f9f2e20d67df
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-16 13:07:43 -0500

    added a home content class and div to HomePage.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 export default function HomePage(){
     return(
         <div className="homePage">
-            <h1>Welcome to the Home page</h1>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+            <div className="homeContent">
+                <h1>CashCamp</h1>
+                <h3>Guide your wealth to the peak!</h3>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
+            </div>
         </div>
     )
 }
