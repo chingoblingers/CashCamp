cf125a14eb6dd1311683cfc21569d1033986a72c
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-16 12:38:43 -0500

    created homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function HomePage(){
+    return(
+        <>
+            <h1>Welcome to the Home page</h1>
+            <Link to="/login"> Login </Link>
+            <Link to="/signup"> Sign Up </Link>
+        </>
+    )
+}
