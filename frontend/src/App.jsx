b7f72fe671f8652cdde962f20fceb9f496bf612f
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-16 12:39:34 -0500

    Made Homepage the default landing page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,10 @@ import DashboardPage from './pages/DashboardPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import SignupPage from './pages/SignupPage.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
+import HomePage from './pages/HomePage.jsx'
 
 const routesArr = [
-  {path: "/", element: <LoginPage/>},
+  {path: "/", element: <HomePage/>},
   {path: "/login", element:<LoginPage/>},
   {path: "/signup", element: <SignupPage/>},
   {element: <ProtectedRoute/>, children: [{path: "/dashboard", element: <DashboardPage/>}]}
