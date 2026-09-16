739a11658f791ff03a727337a33438708a554173
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-16 12:41:01 -0500

    removed whitespace in links.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,8 +4,8 @@ export default function HomePage(){
     return(
         <>
             <h1>Welcome to the Home page</h1>
-            <Link to="/login"> Login </Link>
-            <Link to="/signup"> Sign Up </Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
         </>
     )
 }
