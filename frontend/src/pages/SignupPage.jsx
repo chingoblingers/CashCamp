3430ecd162b50fc34506ab9b369de62b0f0ec9a4
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-16 14:41:44 -0500

    styled signup page.

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -22,18 +22,20 @@ export default function SignupPage(){
 
     return (
         <div className='signupPage'>
-        <h1> Sign up here to create your account! </h1>
-        <Link to="/login"> click here to login if you have an account already! </Link>
-        <form onSubmit={handleSubmit}>
-        <label htmlFor="signupName">Name</label>    
-        <input type="text" name="name" id="signupName" placeholder="Tyler" required onChange={(e)=>setName(e.target.value)} value={name}/>
-        <label htmlFor="signupEmail">Email</label>
-        <input type="email" name="email" id="signupEmail" placeholder="tyler@emailsite.com" required onChange={(e)=>setEmail(e.target.value)} value={email}/>
-        <label htmlFor="signupPassword">Password</label>
-        <input type="password" name="password" id="signupPassword" placeholder="Password123" required onChange={e=>setPassword(e.target.value)} value={password}/>
-        <button type="submit"> Submit </button>
-        {error && <p>{error}</p>}
-        </form>    
+            <div className='authCard'>   
+                <h1> Sign up here to create your account! </h1>
+                <Link to="/login"> click here to login if you have an account already! </Link>
+                <form onSubmit={handleSubmit}>
+                <label htmlFor="signupName">Name</label>    
+                <input type="text" name="name" id="signupName" placeholder="Tyler" required onChange={(e)=>setName(e.target.value)} value={name}/>
+                <label htmlFor="signupEmail">Email</label>
+                <input type="email" name="email" id="signupEmail" placeholder="tyler@emailsite.com" required onChange={(e)=>setEmail(e.target.value)} value={email}/>
+                <label htmlFor="signupPassword">Password</label>
+                <input type="password" name="password" id="signupPassword" placeholder="Password123" required onChange={e=>setPassword(e.target.value)} value={password}/>
+                <button type="submit"> Submit </button>
+                {error && <p>{error}</p>}
+                </form>
+            </div>    
         </div>
     )
 }
