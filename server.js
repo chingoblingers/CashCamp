cec0022c391337bc03a93d48120b2317a32ca155
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-09-17 14:04:04 -0500

    updated port for deployment.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { requireAuth } from "./middleware/requireAuth.js"
 import cors from 'cors'
 
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.use(cors({origin: "http://localhost:5173"}))
 app.use(express.json())
